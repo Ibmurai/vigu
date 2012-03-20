c4cd0d923b36cd619c3a2be78f425a6a75b48d9e
web <web@72642f53-b532-4fd9-9cad-1ceaffd491b0>
2012-03-20 13:59:04 +0000

    Added shutdown handler script. @jers
    git-svn-id: file:///home/svnroot/vigu/trunk@240144 72642f53-b532-4fd9-9cad-1ceaffd491b0

---
+++ handlers/shutdown.php
@@ -0,0 +1,159 @@
+<?php
+
+class ViguErrorHandler {
+	/**
+	 * Contains all logged errors.
+	 *
+	 * @var array[]
+	 */
+	private static $_log = array();
+
+	/**
+	 * Handle any fatal errors.
+	 *
+	 * @return void
+	 */
+	public static function shutdown() {
+		$lastError = error_get_last();
+		$lastLoggedError = self::_getLastLoggedError();
+
+		if ($lastError) {
+			// Make sure that the last error has not already been logged
+			if ($lastLoggedError) {
+				if ($lastLoggedError
+					&& $lastError['file'] == $lastLoggedError['file']
+					&& $lastError['line'] == $lastLoggedError['line']
+					&& $lastError['message'] == $lastLoggedError['message']
+					&& self::_errnoToString($lastError['type']) == $lastLoggedError['level']) {
+					self::_send();
+					return;
+				}
+			}
+
+			self::_logError($lastError['type'], $lastError['message'], $lastError['file'], $lastError['line']);
+		}
+
+		self::_send();
+	}
+
+	/**
+	 * Handle any soft errors.
+	 *
+	 * @param integer $errno      Error number.
+	 * @param string  $errstr     Message.
+	 * @param string  $errfile    File.
+	 * @param integer $errline    Line number.
+	 * @param array   $errcontext Ignored.
+	 *
+	 * @return boolean Returns false to continue error handling by other error handlers.
+	 */
+	public static function error($errno = 0, $errstr = '', $errfile = '', $errline = 0, $errcontext = null) {
+		self::_logError($errno, $errstr, $errfile, $errline, $errcontext, debug_backtrace());
+
+		return false;
+	}
+
+	/**
+	 * Convert an error number to a string.
+	 *
+	 * @param integer $errno
+	 *
+	 * @return string
+	 */
+	private static function _errnoToString($errno) {
+		switch($errno) {
+			case E_ERROR:
+				return 'ERROR';
+			case E_WARNING:
+				return 'WARNING';
+			case E_PARSE:
+				return 'PARSE';
+			case E_NOTICE:
+				return 'NOTICE';
+			case E_CORE_ERROR:
+				return 'CORE ERROR';
+			case E_CORE_WARNING:
+				return 'CORE WARNING';
+			case E_CORE_ERROR:
+				return 'COMPILE ERROR';
+			case E_CORE_WARNING:
+				return 'COMPILE WARNING';
+			case E_USER_ERROR:
+				return 'USER ERROR';
+			case E_USER_WARNING:
+				return 'USER WARNING';
+			case E_USER_NOTICE:
+				return 'USER NOTICE';
+			case E_STRICT:
+				return 'STRICT';
+			case E_RECOVERABLE_ERROR:
+				return 'RECOVERABLE ERROR';
+			case E_DEPRECATED:
+				return 'DEPRECATED';
+			case E_USER_DEPRECATED:
+				return 'USER DEPRECATED';
+			default:
+				return 'UNKNOWN';
+		}
+	}
+
+	/**
+	 * Log an error.
+	 *
+	 * @param integer $errno      The error number.
+	 * @param string  $message    The error message.
+	 * @param string  $file       The file.
+	 * @param integer $line       The line number.
+	 * @param array   $context    The error context (variables available).
+	 * @param array[] $stacktrace The stacktrace, as produced by debug_backtrace().
+	 */
+	private static function _logError($errno, $message, $file, $line, $context = array(), $stacktrace = array()) {
+		array_shift($stacktrace);
+
+		self::$_log[] = array(
+			'timestamp'  => time(),
+			'level'      => self::_errnoToString($errno),
+			'message'    => $message,
+			'file'       => $file,
+			'line'       => $line,
+			'context'    => $context,
+			'stacktrace' => $stacktrace,
+		);
+	}
+
+	/**
+	 * Get the last logged error.
+	 *
+	 * @return array|null
+	 */
+	private static function _getLastLoggedError() {
+		if (!empty(self::$_log)) {
+			return self::$_log[count(self::$_log) - 1];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Send the errors to the Vigu server.
+	 *
+	 * @return void
+	 */
+	private static function _send() {
+		if (!empty(self::$_log)) {
+			$url = 'http://vigu.localhost/api';
+
+			$httpRequest = new HttpRequest($url, HttpRequest::METH_POST);
+			$httpRequest->addPostFields(array('lines' => self::$_log));
+
+			try {
+				$httpRequest->send();
+			} catch (HttpException $e) {
+				// Ignore all errors
+			}
+		}
+	}
+}
+
+register_shutdown_function('ViguErrorHandler::shutdown');
+set_error_handler('ViguErrorHandler::error');
