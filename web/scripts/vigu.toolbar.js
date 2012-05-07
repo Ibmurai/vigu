3c7eea3178e612a0ffe420915d90c9dd2baa6fe0
Johannes Skov Frandsen <localgod@heaven.dk>
2012-05-07 10:34:56 +0200

    Refactored reset button to integrate with the search field.

--- web/scripts/vigu.toolbar.js
+++ web/scripts/vigu.toolbar.js
@@ -43,31 +43,41 @@ Vigu.Toolbar = (function($) {
 		 * @return {undefiend}
 		 */
 		addSearch : function(node) {
-			$('<input type="text">')
-				.attr('name', 'search')
-				.addClass('ui-corner-all')
-				.keypress(function(event) {
-				  if ( event.which == 13 ) {
-					     event.preventDefault();
-					     Vigu.Grid.parameters.path = $('input[name="search"]').val();
-					     Vigu.Grid.reload();
-					   }
+			$('<label>')
+				.append($('<span>')
+						.addClass('ui-icon ui-icon-circle-close')
+						.attr('Title', 'Reset search')
+						.click(function(){
+							Vigu.Grid.parameters.path = '';
+							$('input[name="search"]').val('');
+							Vigu.Grid.reload();
+							$('div[role=toolbar]>label>span').hide();
+						}) 
+						.hide())
+				.append($('<input type="text">')
+					.attr('name', 'search')
+					.addClass('ui-corner-all')
+					.keypress(function(event) {
+					  if (event.which == 13) {
+						     event.preventDefault();
+						     Vigu.Grid.parameters.path = $('input[name="search"]').val();
+						     Vigu.Grid.reload();
+						   }
+						})
+					.click(function(){
+						 this.select();
 					})
-				.click(function(){
-					 this.select();
-				})
-				.focus(function(){
-					 this.select();
-				})
+					.focus(function(){
+						 this.select();
+					})
+					.keydown(function() {
+						if ($('input[name="search"]').val() != '') {
+							$('div[role=toolbar]>label>span').show();
+						} else {
+							$('div[role=toolbar]>label>span').hide();
+						}
+					}))
 				.appendTo(node);
-			$('<button>')
-			.text('Reset')
-			.click(function(){
-				Vigu.Grid.parameters.path = '';
-				$('input[name="search"]').val('');
-				Vigu.Grid.reload();
-			}) 
-			.appendTo(node).button();
 			$('<button>')
 				.text('Reload')
 				.click(function(){
